da51f3a68c3ac38b0979189e610d7867fcb583ec
王劲鹏 <jingpeng.wang@outlook.com>
2022-05-20 11:52:07 +0800

    add register with ip

--- insight-web-backend/src/index.ts
+++ insight-web-backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { spawn } from "child_process";
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -11,7 +12,27 @@ app.get("/", (request, response) => {
 });
 
 app.get("/register", (request, response) => {
-  response.send("insight web backend works!");
+  if (request.query.ip) {
+    const childProcess = spawn(
+      "frida",
+      ["-H", `${request.query.ip}`, "gadget", "--no-pause", "-l", "_agent.js"],
+      { cwd: "../insight-agent" }
+    );
+    childProcess.stderr.on("data", function (data) {
+      console.error(data.toString());
+    });
+
+    childProcess.stdout.on("data", function (data) {
+      console.log(data.toString());
+    });
+
+    childProcess.on("close", (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
+
+    response.send("insight web backend works!");
+  } else {
+  }
 });
 
 app.listen(8080, () => {
